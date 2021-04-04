ecaccf090a70afd00bf2715007ac45fcc1203887
oussama-gif <66790652+oussama-gif@users.noreply.github.com>
2021-04-04 18:50:10 +0100

    First Comment

---
+++ script.js
@@ -0,0 +1,25 @@
+var option = document.querySelectorAll("#nav .option");
+
+
+option.forEach(span => {
+    span.addEventListener("click",(e) => {
+        console.log("1");
+        
+    });
+});
+
+console.log("hello");
+
+
+/*
+var option = document.getElementById("aze");
+
+
+    option.addEventListener('click', function(){
+        window.scrollTo(0,500);
+    });
+
+function myFunction(){
+
+    window.scrollTo(0,this.dataset.scroll);
+}*/
